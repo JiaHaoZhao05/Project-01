287f54c04711226471d0066ad2a212316d4c6e90
JiaHaoZhao05 <jia.hao.zhao@estudiantat.upc.edu>
2025-02-17 12:47:10 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,7 @@ int main ()
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
 
 	Vector2 ballPosition = { (float)1920 / 2, (float)1080 / 2 };
-	int t = 1;
-
-	float currentTime = GetTime();
-	float lastTime = 0.0f;
-	int gravityactive = 0;
-	float gravityacc = 0;
+	static float gravityacc = 0;
 	int Wtrigger = 0;
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -47,39 +42,36 @@ int main ()
 		if (IsKeyDown('D') || IsKeyDown(KEY_RIGHT)) ballPosition.x += 5.0f;
 		if (IsKeyDown('A') || IsKeyDown(KEY_LEFT)) ballPosition.x -= 5.0f;
 		if ((IsKeyDown('W') || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && Wtrigger == 1) {
-			
 			Wtrigger = 0;
 			ballPosition.y -= 10.0f;
-			gravityacc = -20;
+			gravityacc = -20.0f;
 		}
-		
+		if (IsKeyDown('S') || IsKeyDown(KEY_DOWN)) gravityacc += 3.0f;
 		
 		
 		if (ballPosition.y < 750.0f) {
-
-			gravityactive = 1;
 			ballPosition.y += gravityacc;
 			
-<<<<<<< HEAD
 			if (gravityacc < 15.0f) {
 				gravityacc += 0.8f;
 			}
-			else if (gravityacc > gravityacc) {
-				gravityacc = 15;
+			else if (gravityacc > 15.0f) {
+				gravityacc = 15.0f;
 			}
-=======
-			
-			gravityacc += 1.0f;
->>>>>>> 22a55e6da9edd7854715d92a2eb8c4266d172df3
-			
-
+		}
+		if (CheckCollisionCircleLine(ballPosition, 50, { 1850,600 }, { 900,600 })) {
+			if (ballPosition.y > 550 && gravityacc > 0) {
+				ballPosition.y = ballPosition.y;
+				gravityacc = 0;
+				Wtrigger = 1;
+			}
 		}
 		else if(ballPosition.y >= 750.0f) {
 			ballPosition.y = 750.0f;
 			gravityacc = 0;
-			gravityactive = 0;
 			Wtrigger = 1;
 		}
+		
 		//----------------------------------------------------------------------------------
 
 		// Draw
@@ -93,7 +85,7 @@ int main ()
 		DrawText(TextFormat("Wtrigger: %i", Wtrigger), 10, 50, 20, DARKGRAY);
 
 		DrawCircleV(ballPosition, 50, MAROON);
-
+		DrawLine(1850, 600, 900, 600, GREEN);
 		EndDrawing();
 		//----------------------------------------------------------------------------------
 	}
