bbed0a4b9d4bdc94e41c11de74938486a96f64e1
JiaHaoZhao05 <jia.hao.zhao@estudiantat.upc.edu>
2025-02-20 19:46:20 +0100

    Underblock collision

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "raylib.h"
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+struct player {
+	Rectangle box;
+	float gravity;
+	int jumps;
+	Texture skin;
+
+};
 
 int main()
 {
@@ -22,20 +29,10 @@ int main()
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	struct player {
-
-		float x;
-		float y;
-		float height;
-		float width;
-		Texture skin;
-
-	};
-
-	struct player Reina = { 0,0,128,64,LoadTexture("Hormiga_Prueva.png") };
-	float gravity = 0;
-	int jumps = 0;
-
+	
+	struct player Reina = { 0,0,64,128,0,0,LoadTexture("Hormiga_Prueva.png") };
+	Rectangle tester = { 1000, 600, 200, 10};
+	Rectangle Floor[10];
 	// game loop
 	while (!WindowShouldClose())// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
@@ -47,49 +44,51 @@ int main()
 
 		// Update
 		//----------------------------------------------------------------------------------
-
+		
 		//Movement -------------------------------------------------------------------------
-		if (IsKeyDown('D') || IsKeyDown(KEY_RIGHT)) Reina.x += 5.0f;
-		if (IsKeyDown('A') || IsKeyDown(KEY_LEFT)) Reina.x -= 5.0f;
-		if ((IsKeyDown('W') || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && jumps > 0 && gravity >= 0) {
-			jumps--;
-			Reina.y -= 10.0f;
-			gravity = -20.0f;
+		if (IsKeyDown('D') || IsKeyDown(KEY_RIGHT)) Reina.box.x += 5.0f;
+		if (IsKeyDown('A') || IsKeyDown(KEY_LEFT)) Reina.box.x -= 5.0f;
+		if ((IsKeyDown('W') || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && Reina.jumps > 0 && Reina.gravity >= 0) {
+			Reina.jumps--;
+			Reina.box.y -= 10.0f;
+			Reina.gravity = -20.0f;
 		}
 		if (IsKeyDown('S') || IsKeyDown(KEY_DOWN)) {
-			gravity += 3.0f;
-			Reina.y += 1.0f;
+			Reina.gravity += 3.0f;
+			Reina.box.y += 1.0f;
 		}
 
 		
 		//----------------------------------------------------------------------------------
-
-		//Physics --------------------------------------------------------------------------
-		if (Reina.y < 686.0f) {
-
-			Reina.y += gravity;
-
-			if (gravity < 15.0f) {
-				gravity += 0.8f; //gravity acceleration
-			}
-			else if (gravity > 15) {
-				gravity = 15; //terminal velocity
-			}
-
-		}
-		else if (Reina.y >= 686.0f) {
-			Reina.y = 686.0f;
-			gravity = 0;
-			jumps = 2;
-		}
-		if (Reina.x < 0) {
-			Reina.x = 0;
-		}
-		else if (Reina.x > 1280) {
-			Reina.x = 1280;
-		}
-
 		
+		//Physics --------------------------------------------------------------------------
+		if (Reina.box.y >= 686.0f) {
+			Reina.box.y = 686.0f;
+			Reina.gravity = 0;
+			Reina.jumps = 2;
+		}
+		if (Reina.box.x < 0) {
+			Reina.box.x = 0;
+		}
+		else if (Reina.box.x > 1280) {
+			Reina.box.x = 1280;
+		}
+		
+		if (CheckCollisionRecs(Reina.box, tester)) {
+			if (Reina.gravity > 0) {
+				Reina.gravity = 0;
+				Reina.jumps = 2;
+			}
+		}
+		else {
+			if (Reina.gravity < 15.0f) {
+				Reina.gravity += 0.8f; //gravity acceleration
+			}
+			else if (Reina.gravity > 15) {
+				Reina.gravity = 15; //terminal velocity
+			}
+		}
+		Reina.box.y += Reina.gravity;
 		//----------------------------------------------------------------------------------
 
 		// Draw
@@ -98,23 +97,23 @@ int main()
 
 		ClearBackground(RAYWHITE);
 
-		DrawText(TextFormat("pos y: %f", Reina.y), 10, 10, 20, DARKGRAY);
-		DrawText(TextFormat("Jumps: %i", jumps), 10, 50, 20, DARKGRAY);
+		DrawText(TextFormat("pos y: %f", Reina.box.y), 10, 10, 20, DARKGRAY);
+		DrawText(TextFormat("Jumps: %i", Reina.jumps), 10, 50, 20, DARKGRAY);
 
 		
 
-		DrawRectangle(1000, 600, 200, 10,BLACK);
+		DrawRectangle(tester.x,tester.y,tester.width,tester.height,BLACK);
 
-		DrawTexture(Reina.skin, Reina.x, Reina.y, WHITE);
+		DrawTexture(Reina.skin, Reina.box.x, Reina.box.y, WHITE);
 
 		DrawLine(1850, 600, 900, 600, GREEN);
 
-		if (IsKeyDown('A') || IsKeyDown(KEY_RIGHT)) Reina.skin = LoadTexture("Hormiga_IZQUIERDA_Prueva.png");
-		if (IsKeyDown('D') || IsKeyDown(KEY_LEFT)) Reina.skin = LoadTexture("Hormiga_Prueva.png");
-		if ((IsKeyDown('W') || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && jumps > 0 && gravity >= 0) {
-			jumps--;
-			Reina.y -= 10.0f;
-			gravity = -20.0f;
+		if (IsKeyDown('A') || IsKeyDown(KEY_RIGHT)) Reina.skin = LoadTexture("Hormiga_Prueva.png");
+		if (IsKeyDown('D') || IsKeyDown(KEY_LEFT)) Reina.skin = LoadTexture("Hormiga_IZQUIERDA_Prueva.png");
+		if ((IsKeyDown('W') || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && Reina.jumps > 0 && Reina.gravity >= 0) {
+			Reina.jumps--;
+			Reina.box.y -= 10.0f;
+			Reina.gravity = -20.0f;
 		}
 
 		EndDrawing();
