8c72986193896742980bdc0d772a5002f516c696
JiaHaoZhao05 <jia.hao.zhao@estudiantat.upc.edu>
2025-04-21 16:13:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,9 @@ int main() {
 				if (Mario.position.x > 1200 - Mario.GetRect().width) { // border right
 					Mario.position.x = 1200 - Mario.GetRect().width;
 				}
+				if (Mario.position.y < 0) { //death by falling
+					Mario.lives = 0;
+				}
 				//Resolver bug de break_block
 				for (int i = 0; i < level1.collisions.size(); ++i) {
 					level1.collisions[i]->SolveBreakBug();
@@ -240,7 +243,7 @@ int main() {
 			if (Mario.lives <= 0) {
 				PlaySound(over);
 			}
-			while (!WindowShouldClose() && Mario.lives <= 0 && !menu) {
+			while (!WindowShouldClose() && Mario.lives <= 0 && !menu) { // gameover screen
 				BeginDrawing();
 				ClearBackground(RAYWHITE);
 
@@ -257,7 +260,7 @@ int main() {
 			if (distance >= 12300) {
 				PlaySound(win);
 			}
-			while (!WindowShouldClose() && distance >= 12300 && !menu) {
+			while (!WindowShouldClose() && distance >= 12300 && !menu) { // win screen
 				BeginDrawing();
 				ClearBackground(RAYWHITE);
 
@@ -270,67 +273,6 @@ int main() {
 				EndDrawing();
 			}
 		}
-<<<<<<< HEAD
-=======
-
-			
-		/*for (int i = 0; i < level1.block_question.size(); ++i) {
-				level1.block_question[i]->CollidingWithPlayer(Mario.GetRect());
-		}*/
-
-		for (int i = 0; i < level1.collisions.size(); ++i) { // collision with map
-				Mario.Colliding(/*pasar rectangulo con una funcion rectangle <Block> */*level1.collisions[i]);
-		}
-
-		// NO MAS COLISIONES A PARTIR DE AQUI
-
-		Mario.Movement();
-		//level1.GenerateMap(map);
-
-		for (int i = 0; i < EnemiesLvl1.goombas.size(); ++i) {
-			EnemiesLvl1.goombas[i]->movement();
-		}
-
-
-		for (int a = 0; a < level1.collisions.size(); ++a) { // draw map
-				DrawTextureV(level1.collisions[a]->texture, level1.collisions[a]->pos, WHITE);
-		}
-		
-		if (distance >= 12254) {
-
-		}
-
-		DrawText(TextFormat("Distance: %d", distance), 10, 90, 20, BLACK);
-		DrawText(TextFormat("Lives: %d", Mario.lives), 10, 110, 20, BLACK);
-		EndDrawing();
-	}
-	StopSound(bgm.bgMusic);
-	Texture2D overScreen = LoadTexture("resources/gameover_screen.png");
-	Sound over = LoadSound("resources/Music/Game Over.mp3");
-	if (Mario.lives <= 0) {
-		PlaySound(over);
-	}
-	while (!WindowShouldClose() && Mario.lives <= 0) {
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-
-		DrawTexture(overScreen, 0, 0, WHITE);
-
-		EndDrawing();
-	}
-	Texture2D winScreen = LoadTexture("resources/win_screen.png");
-	Sound win = LoadSound("resources/Music/Level Complete.mp3");
-	if (distance >= 12300) {
-		PlaySound(win);
-	}
-	while (!WindowShouldClose() && distance>=12300) {
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-
-		DrawTexture(winScreen, 0, 0, WHITE);
-
-		EndDrawing();
->>>>>>> 7e84f5345a5b5dd832e6abdaac1a3c0b301eb68d
 	}
 	// cleanup
 	// unload our texture so it can be cleaned up
